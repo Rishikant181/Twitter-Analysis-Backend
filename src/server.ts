8773b988d42d54f4e55deea1316f7997ca841242
Rishikant181 <rishikantsahu181@gmail.com>
2023-02-20 13:44:55 +0530

    Initialized empty express server with a test endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("Hello World");
+// PACKAGE LIBS
+import express from 'express';
+
+// Creating new express app instance
+const app = express();
+
+// Setting up express server
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
+
+// Setting up test endpoint
+app.get('/', (req, res) => {
+    res.sendStatus(200);
+});
