85f7859ba40663c37240744220e00d1ea704d71c
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-03-09 11:04:44 +0530

    Added endpoint to fetch user details

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,18 @@
 // PACKAGE
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 // SERVICES
 import { UserService } from './user.service';
 
+// ENTITIES
+import { User } from './entities/user.entity';
+
 @Controller('user')
 export class UserController {
 	constructor(private readonly userService: UserService) { }
+
+	@Get(':id')
+	find(@Param('id') id: string): Promise<User> {
+		return this.userService.find(id);
+	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,9 +2,25 @@
 import { Injectable } from '@nestjs/common';
 import { Rettiwt } from 'rettiwt-api';
 
+// ENTITIES
+import { User } from './entities/user.entity';
+
 @Injectable()
 export class UserService {
-	find(id: string) {
-		
+	/**
+	 * @param id The id/username of the twitter user.
+	 * @returns The details of the twitter user with the given id/username.
+	 */
+	find(id: string): Promise<User> {
+		// If username is provided
+        if(isNaN(Number(id))) {
+            // Fetching and returning the details using username
+            return Rettiwt().users.getUserDetails(id);
+        }
+        // If id is provided
+        else {
+            // Fetching and returning the details using id
+            return Rettiwt().users.getUserDetailsById(id);
+        }
 	}
 }
