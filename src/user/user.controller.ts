ebc475442228ae0b8957631930d7b70cd1623b8c
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-03-09 14:00:52 +0530

    Added cursored data entity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,13 @@
 // PACKAGE
 import { Controller, Get, Param } from '@nestjs/common';
+import { Query, Req } from '@nestjs/common/decorators';
 
 // SERVICES
 import { UserService } from './user.service';
 
 // ENTITIES
 import { User } from './entities/user.entity';
+import { CursoredData } from '../entities/cursored.entity';
 
 @Controller('user')
 export class UserController {
@@ -14,5 +16,10 @@ export class UserController {
 	@Get(':id')
 	find(@Param('id') id: string): Promise<User> {
 		return this.userService.find(id);
+	}
+
+	@Get(':id/followers')
+	followers(@Req() request: Request, @Param('id') id: string, @Query('count') count?: number, @Query('cursor') cursor?: string): Promise<CursoredData<User>> {
+		
 	}
 }
