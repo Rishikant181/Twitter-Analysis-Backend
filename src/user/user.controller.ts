10012b7d6c53f54ff8782006af330423f23b7444
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-03-10 00:47:22 +0530

    Added endpoint to fetch user follow data

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -19,6 +19,11 @@ export class UserController {
 
 	@Get(':id/followers')
 	followers(@Req() request: Request, @Param('id') id: string, @Query('count') count?: number, @Query('cursor') cursor?: string): Promise<CursoredData<User>> {
-		
+		return this.userService.findFollowers(id, count, cursor);
+	}
+
+	@Get(':id/following')
+	following(@Req() request: Request, @Param('id') id: string, @Query('count') count?: number, @Query('cursor') cursor?: string): Promise<CursoredData<User>> {
+		return this.userService.findFollowing(id, count, cursor);
 	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,31 @@
 // PACKAGE
 import { Inject, Injectable, Scope } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
-import { Rettiwt } from 'rettiwt-api';
+import { Rettiwt, DataErrors } from 'rettiwt-api';
 
 // ENTITIES
-import { User } from './entities/user.entity';
+import { User, CursoredData } from './entities/user.entity';
 
 /**
  * This service is request-scoped since a new instance is created for every request, and the associated cookies are used to fetch the data.
  */
 @Injectable({ scope: Scope.REQUEST })
 export class UserService {
+    /** The cookie string to use for authenticatin Rettiwt instance. */
+    private cookie: string;
+    
     /**
      * @param request The oncoming HTTP request from the client.
      */
-    constructor(@Inject(REQUEST) private request: Request) {}
+    constructor(@Inject(REQUEST) private request: Request) {
+        this.cookie = request.headers['cookie'];
+    }
 
 	/**
 	 * @param id The id/username of the twitter user.
 	 * @returns The details of the twitter user with the given id/username.
 	 */
-	find(id: string): Promise<User> {
+	async find(id: string): Promise<User> {
 		// If username is provided
         if(isNaN(Number(id))) {
             // Fetching and returning the details using username
@@ -32,4 +37,102 @@ export class UserService {
             return Rettiwt().users.getUserDetailsById(id);
         }
 	}
+
+    /**
+     * Get the followers of the Twitter user with the given id
+     * 
+     * @param id The id of the twitter user.
+     * @param count The number of followers to fetch, must be >= 40 when no cursor is provided.
+     * @param cursor The cursor to next batch.
+     * @returns The list of follower of the twitter user with the given id.
+     */
+    async findFollowers(id: string, count: number, cursor: string): Promise<CursoredData<User>> {
+        let followers: CursoredData<User> = {
+            list: [],
+            next: { value: cursor }
+        };
+        let total: number = 0;                                          // To store the total number of data fetched
+        let batchSize: number = 100;                                    // To store the number of data to fetch at once
+
+        // Fetching batch-wise, as long as total data fetched is less than required
+        do {
+            // For last batch, set batch size to amount of data remaining
+            /** 
+             * If the amount of data remaining to fetch ( = count - total) is <= batchSize, this implies this is the last batch.
+             * So the batch size is reduced to the amount of data remaining to fetch
+             */
+            batchSize = ((count - total) <= batchSize) ? (count - total) : batchSize;
+
+            // Fetching a single batch
+            let data = await Rettiwt(this.cookie).users.getUserFollowers(id, batchSize, followers.next.value);
+
+            // If no additional data found
+            if (!data.list.length) {
+                break;
+            }
+
+            // Concatenating data
+            followers.list = followers.list.concat(data.list);
+            followers.next = data.next;
+
+            // Incrementing total data fetched
+            total = followers.list.length;
+        } while (total < count);
+
+        // If no followers found
+        if (!followers.list.length) {
+            throw new Error(DataErrors.NoFollowsFound);
+        }
+
+        return followers;
+    }
+
+    /**
+     * Get the following of the Twitter user with the given id
+     * 
+     * @param id The id of the twitter user
+     * @param count The number of following to fetch, must be >= 40 when no cursor is provided
+     * @param cursor The cursor to next batch
+     * @returns The list of following of the twitter user with the given id
+     */
+    async findFollowing(id: string, count: number, cursor: string): Promise<CursoredData<User>> {
+        let following: CursoredData<User> = {
+            list: [],
+            next: { value: cursor }
+        };
+        let total: number = 0;                                          // To store the total number of data fetched
+        let batchSize: number = 100;                                    // To store the number of data to fetch at once
+
+        // Fetching batch-wise, as long as total data fetched is less than required
+        do {
+            // For last batch, set batch size to amount of data remaining
+            /** 
+             * If the amount of data remaining to fetch ( = count - total) is <= batchSize, this implies this is the last batch.
+             * So the batch size is reduced to the amount of data remaining to fetch
+             */
+            batchSize = ((count - total) <= batchSize) ? (count - total) : batchSize;
+
+            // Fetching a single batch
+            let data = await Rettiwt(this.cookie).users.getUserFollowing(id, batchSize, following.next.value);
+
+            // If no additional data found
+            if (!data.list.length) {
+                break;
+            }
+
+            // Concatenating data
+            following.list = following.list.concat(data.list);
+            following.next = data.next;
+
+            // Incrementing total data fetched
+            total = following.list.length;
+        } while (total < count);
+
+        // If no following found
+        if (!following.list.length) {
+            throw new Error(DataErrors.NoFollowsFound);
+        }
+
+        return following;
+    }
 }
