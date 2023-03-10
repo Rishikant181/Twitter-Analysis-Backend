f4347f3c0023cd9ac5e1407d1ef32596a97c8f56
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-03-10 12:20:38 +0530

    Added tweet query dto

--- src/tweet/tweet.controller.ts
+++ src/tweet/tweet.controller.ts
@@ -9,10 +9,18 @@ import { TweetService } from './tweet.service';
 import { Tweet, CursoredData } from './entities/tweet.entity';
 import { User } from '../user/entities/user.entity';
 
+// DTOs
+import { TweetQueryDto } from './dto/tweet-query.dto';
+
 @Controller('tweet')
 export class TweetController {
 	constructor(private readonly tweetService: TweetService) { }
 	
+	@Get('/')
+	findTweets(@Query() query: TweetQueryDto) {
+		return "Hello World";
+	}
+
 	@Get(':id')
 	find(@Param('id') id: string): Promise<Tweet> {
 		return this.tweetService.find(id);
