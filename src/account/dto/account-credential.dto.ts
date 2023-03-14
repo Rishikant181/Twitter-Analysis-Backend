2f8748d33ead8185156363cfc08a08e148802f6c
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-03-14 20:47:29 +0530

    Added AccountCredentialDto

---
+++ src/account/dto/account-credential.dto.ts
@@ -0,0 +1,20 @@
+// PACKAGES
+import { IsAlpha, IsAlphanumeric, IsEmail, IsString } from 'class-validator'
+
+/**
+ * The credentials used to login to an account.
+ */
+export class AccountCredentialDto {
+    /** The email associated with the Twitter account. */
+    @IsString()
+    @IsEmail()
+    email: string;
+
+    /** The username of the Twitter account's user. */
+    @IsAlphanumeric()
+    userName: string;
+
+    /** The password to the Twitter account. */
+    @IsString()
+    password: string;
+}
