4d6e7074aefdafaeb571ddc4c905294d580f3407
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-03-27 12:33:04 +0530

    Added cookie parser middleware

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,22 @@
 // PACKAGES
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as CookieParser from 'cookie-parser';
 
 // MODULES
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+	// Initializing new NestJS app
 	const app = await NestFactory.create(AppModule);
+
+	// Adding global pipes
 	app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+	// Adding middlewares
+	app.use(CookieParser());
+
+	// Listening for HTTP requests
 	await app.listen(3000);
 }
 
