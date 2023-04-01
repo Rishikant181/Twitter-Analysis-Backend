1e4fc22a9cc1fc52f5f3a29a0aae1a6a26730d38
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-04-01 16:06:37 +0530

    Added cursored list dto

---
+++ src/twitter/dto/cursored-list.dto.ts
@@ -0,0 +1,39 @@
+// PACKAGES
+import { ICursoredData } from 'rettiwt-api';
+
+/**
+ * The cursor to the batch of data to be fetched.
+ */
+export class Cursor {
+    /** The cursor string. */
+    value: string;
+
+    /**
+     * @param cursorStr The cursor string.
+     */
+    constructor(cursorStr: string) {
+        this.value = cursorStr;
+    }
+}
+
+/**
+ * The data that us fetched batch-wise along with a cursor.
+ *
+ * @typeParam Type - The type of data present in the list.
+ */
+export class CursoredData<T> implements ICursoredData<T> {
+    /** The list of data of the given type. */
+    list: T[];
+
+    /** The cursor to the next batch of data. */
+    next: Cursor;
+
+    /**
+     * @param list The data list to be stored.
+     * @param next The cursor string to the next batch.
+     */
+    constructor(list: T[], next: string) {
+        this.list = list;
+        this.next = new Cursor(next)
+    }
+}
