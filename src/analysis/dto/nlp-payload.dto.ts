a811662dd8dca0f8acd0e74ba7f9d0efd037348b
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-04-01 10:59:52 +0530

    Added IDocument implementation

--- src/analysis/dto/nlp-payload.dto.ts
+++ src/analysis/dto/nlp-payload.dto.ts
@@ -2,6 +2,27 @@
 import { IPayload, IDocument, EncodingType, ContentType } from '../interface/nlp-payload.interface';
 
 /**
+ * Represents the input to API methods.
+ */
+export class NlpDocumentDto implements IDocument {
+    /** The content type of the document. If the type is not set or is TYPE_UNSPECIFIED, returns an INVALID_ARGUMENT error. */
+    type: ContentType;
+
+    /** The content of the input in string format. */
+    content: string;
+
+    /**
+     * Initializes the document representation of the given text.
+     * 
+     * @param content The text to be represented as a document.
+     */
+    constructor(content: string) {
+        this.type = ContentType.PLAIN_TEXT;
+        this.content = content;
+    }
+};
+
+/**
  * The payload to be sent for analyzing entity sentiment.
  */
 export class NlpPayLoadDto implements IPayload {
@@ -18,10 +39,7 @@ export class NlpPayLoadDto implements IPayload {
         /** The concatenated string, containing all the input texts as a paragraph. */
         let concatString: string = texts.join('');
 
-        this.document = {
-            type: ContentType.PLAIN_TEXT,
-            content: concatString
-        };
+        this.document = new NlpDocumentDto(concatString);
         this.encodingType = EncodingType.UTF8;
     }
 }
