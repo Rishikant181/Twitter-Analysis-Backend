46474539e883386e03b8613c8c2ba6771feefb85
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-04-24 22:00:27 +0530

    Setup controller properly

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,12 +1,19 @@
+// PACKAGES
 import { Body, Controller, Post } from '@nestjs/common';
+
+// SERVICES
 import { AccountService } from './account.service';
+
+// DTOs
+import { AuthKeyDto } from 'src/twitter/auth/dto/auth-key.dto';
+import { AuthCredentialDto } from 'src/twitter/auth/dto/auth-credential.dto';
 
 @Controller('account')
 export class AccountController {
-	constructor(private readonly accountService: AccountService) {}
+	constructor(private readonly accountService: AccountService) { }
 
 	@Post('login')
-	login(@Body() credentials: unknown): Promise<undefined> {
+	login(@Body() credentials: AuthCredentialDto): Promise<AuthKeyDto> {
 		return this.accountService.login(credentials);
 	}
 }
