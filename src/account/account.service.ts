55333ef7adffeeec9f7778822695c0bcf433b98d
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-04-24 21:53:35 +0530

    login method of account service now works properly

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,6 +1,8 @@
 // PACKAGES
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, HttpException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 // MODELS
 import { Account } from './schema/account.schema';
@@ -9,13 +11,15 @@ import { Account } from './schema/account.schema';
 import { AuthCredentialDto } from 'src/twitter/auth/dto/auth-credential.dto';
 import { AuthKeyDto } from 'src/twitter/auth/dto/auth-key.dto';
 import { AuthService } from 'src/twitter/auth/auth.service';
+import { HttpStatusCode } from 'axios';
 
 @Injectable()
 export class AccountService {
 	constructor(
 		@Inject(ConfigService) private configService: ConfigService,
 		@Inject(AuthService) private auth: AuthService,
-	) {}
+		@InjectModel(Account.name) private accountModel: Model<Account>
+	) { }
 
 	/**
 	 * Login into the twitter account with given credentials.
@@ -23,6 +27,26 @@ export class AccountService {
 	 * @param credential The twitter account credentials.
 	 */
 	async login(credential: AuthCredentialDto): Promise<AuthKeyDto> {
-		return this.auth.login(credential);
+		// Getting the account details of the account with the given email
+		const account: Account = await this.accountModel.findOne({ email: credential.email });
+
+		// If account does not exist, then login to twitter
+		if (!account) {
+			return await this.auth.login(credential);
+		}
+		// If account exists, check password
+		else if (credential.password == account.password) {
+			return {
+				kdt: account.kdt,
+				twid: account.twid,
+				ct0: account.ct0,
+				// eslint-disable-next-line @typescript-eslint/naming-convention
+				auth_token: account.auth_token
+			};
+		}
+		// If wrong password
+		else {
+			throw new HttpException('Wrong password!', HttpStatusCode.Unauthorized);
+		}
 	}
 }
