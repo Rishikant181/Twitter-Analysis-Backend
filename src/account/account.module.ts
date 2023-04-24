398f72e900e5cfb755a1906cbfe88d88e3318cc2
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-04-24 21:22:02 +0530

    Added mongodb connection

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AccountService } from './account.service';
 import { AccountController } from './account.controller';
 
 @Module({
-	imports: [ConfigModule],
+	imports: [ConfigModule, MongooseModule.forRootAsync({
+		imports: [ConfigModule],
+		useFactory: (configService: ConfigService) => ({
+			uri: configService.get<string>('ATLAS_URL'),
+		}),
+		inject: [ConfigService]
+	})],
 	controllers: [AccountController],
 	providers: [AccountService],
 })
-export class AccountModule {}
+export class AccountModule { }
